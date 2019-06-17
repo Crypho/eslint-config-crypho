d3701d86f802df7b07078a4455eab8dc84fdca56
Wichert Akkerman <wichert@wiggy.net>
2019-06-17 15:47:56 +0200

    Typescript fixes

--- typescript.js
+++ typescript.js
@@ -1,9 +1,17 @@
 module.exports = {
-  extends: ["plugin:@typescript-eslint/recommended"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint"
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "2018",
     sourceType: "module"
+  },
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": false,
+    // Typescript will almost always infer this correctly
+    "@typescript-eslint/explicit-function-return-type": false
   },
   overrides: [
     {
