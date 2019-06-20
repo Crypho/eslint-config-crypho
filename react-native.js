5ccd87ad5fd3fc3f062863350d0253318dae4558
Wichert Akkerman <wichert@wiggy.net>
2019-06-20 09:00:38 +0200

    Add react-native configuration

---
+++ react-native.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: ['plugin:react/recommended', 'prettier/react', 'plugin:react-native/all'],
+  plugins: ['react', 'react-native', 'babel'],
+  env: {
+    es6: true,
+    'react-native/react-native': true
+  },
+  globals: {
+    require: false,
+    process: false,
+    global: false,
+  },
+  parser: 'babel-eslint',
+  overrides: [
+    {
+      files: ['**/*.jsx', '**/*.tsx'],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      rules: {
+        'unicorn/filename-case': 'off',
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+    },
+  ],
+}
